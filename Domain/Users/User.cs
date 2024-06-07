009cd3db76c002fa3ca23a8c6bc2c2795a08b275
Grabtot <dneshotkin@gmail.com>
2024-06-07 20:04:55 +0300

    created user model (closes #4)

---
+++ Domain/Users/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PizzaApi.Domain.Users
+{
+    public class User : IdentityUser<Guid>
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
