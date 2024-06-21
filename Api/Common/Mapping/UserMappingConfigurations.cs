9b451aa265e932643574b04abd1a4c762e99d13c
Grabtot <dneshotkin@gmail.com>
2024-06-21 13:49:00 +0300

    Created `Name` property in user to use it instead of using `UserName` in `IdentityUser` class

--- Api/Common/Mapping/UserMappingConfigurations.cs
+++ Api/Common/Mapping/UserMappingConfigurations.cs
@@ -9,7 +9,6 @@ namespace PizzaApi.Api.Common.Mapping
         public void Register(TypeAdapterConfig config)
         {
             config.NewConfig<User, UserResponse>()
-                .Map(response => response.Name, user => user.UserName)
                 .Map(response => response, user => user);
         }
     }
---
+++ Domain/Users/Events.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+namespace PizzaApi.Domain.Users
+{
+    public record UserCreatedEvent(User User) : INotification;
+}
