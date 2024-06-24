d9a2d8e0029233ffd6af6e0a8a9fc8021df612ab
Grabtot <dneshotkin@gmail.com>
2024-06-24 12:56:43 +0300

    Rename getUser endpoit to info

--- Application/Users/Queries/UserInfo/UserInfoQueryHandler.cs
+++ Application/Users/Queries/UserInfo/UserInfoQueryHandler.cs
@@ -5,12 +5,12 @@ using PizzaApi.Domain.Users;
 
 namespace PizzaApi.Application.Users.Queries
 {
-    public class GetUserQueryHandler(IUsersRepository usersRepository)
-        : IRequestHandler<GetUserQuery, ErrorOr<User>>
+    public class UserInfoQueryHandler(IUsersRepository usersRepository)
+        : IRequestHandler<UserInfoQuery, ErrorOr<User>>
     {
         private readonly IUsersRepository _usersRepository = usersRepository;
 
-        public async Task<ErrorOr<User>> Handle(GetUserQuery query, CancellationToken cancellationToken)
+        public async Task<ErrorOr<User>> Handle(UserInfoQuery query, CancellationToken cancellationToken)
         {
             User? user = await _usersRepository.FindAsync(query.Id);
 
