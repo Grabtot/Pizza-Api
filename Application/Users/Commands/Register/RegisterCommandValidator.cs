b3a8c463c9b1fd4e213b0eabe4ac2fa704aa9ad9
Grabtot <dneshotkin@gmail.com>
2024-06-24 16:13:43 +0300

    Added password validatror

--- Application/Users/Commands/Register/RegisterCommandValidator.cs
+++ Application/Users/Commands/Register/RegisterCommandValidator.cs
@@ -1,14 +1,16 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 using PizzaApi.Application.Common.Validation;
 
 namespace PizzaApi.Application.Users.Commands.Register
 {
     public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
     {
-        public RegisterCommandValidator()
+        public RegisterCommandValidator(PasswordOptions options)
         {
             RuleFor(c => c.Email).EmailAddress().NotNull();
-            RuleFor(c => c.Password).NotNull().NotEmpty();
+            RuleFor(c => c.Password).Password(options);
             RuleFor(c => c.Name).UserName();
         }
     }
