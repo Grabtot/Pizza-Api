a669afde5e78333c4c8f5728fb17c00eff2c4363
Grabtot <dneshotkin@gmail.com>
2024-06-24 19:20:00 +0300

    add deleteAccount endpoint closes #10

---
+++ Application/Users/Commands/DelateUser/DelateUserCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using ErrorOr;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using PizzaApi.Domain.Users;
+
+namespace PizzaApi.Application.Users.Commands.DelateUser
+{
+    public class DelateUserCommandHandler(UserManager<User> userManager)
+        : IRequestHandler<DelateUserCommand, ErrorOr<Success>>
+    {
+        private readonly UserManager<User> _userManager = userManager;
+
+        public async Task<ErrorOr<Success>> Handle(DelateUserCommand command, CancellationToken cancellationToken)
+        {
+            User user = await _userManager.FindByIdAsync(command.UserId.ToString())
+                ?? throw new InvalidOperationException($"User with id {command.UserId} not found");
+
+            IdentityResult result = await _userManager.DeleteAsync(user);
+
+            if (!result.Succeeded)
+                throw new Exception(result.Errors.First().Description);
+
+            return Result.Success;
+        }
+    }
+}
