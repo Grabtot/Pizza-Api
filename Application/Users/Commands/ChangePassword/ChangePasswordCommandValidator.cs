b25149b73e401e3af5d1a36a068c328b1a5ead3e
Grabtot <dneshotkin@gmail.com>
2024-06-24 18:45:10 +0300

    added changePassword endpoint #13

---
+++ Application/Users/Commands/ChangePassword/ChangePasswordCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+using PizzaApi.Application.Common.Validation;
+
+namespace PizzaApi.Application.Users.Commands.ChangePassword
+{
+    public class ChangePasswordCommandValidator : AbstractValidator<ChangePasswordCommand>
+    {
+        public ChangePasswordCommandValidator(PasswordOptions passwordOptions)
+        {
+            RuleFor(c => c.OldPassword).NotEmpty().NotNull();
+            RuleFor(c => c.NewPassword).Password(passwordOptions);
+        }
+    }
+}
--- Application/Users/Queries/ChangeEmail/ChangeEmailQuery.cs
+++ Application/Users/Queries/ChangeEmail/ChangeEmailQuery.cs
@@ -3,5 +3,5 @@ using MediatR;
 
 namespace PizzaApi.Application.Users.Queries.ChangeEmail
 {
-    public record ChangeEmailQuery(string OldEmail, string NewEmail) : IRequest<ErrorOr<Success>>;
+    public record ChangeEmailQuery(Guid UserId, string NewEmail) : IRequest<ErrorOr<Success>>;
 }
