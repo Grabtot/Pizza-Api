ca99d4199d8d8dc8988f115284935605d7490acc
Grabtot <dneshotkin@gmail.com>
2024-06-29 14:06:57 +0300

    added `passwordRequirements` endpoint

---
+++ Application/Users/Queries/PasswordRequirements/PasswordRequirementsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace PizzaApi.Application.Users.Queries.PasswordRequirements
+{
+    public record PasswordRequirementsQuery : IRequest<PasswordOptions>
+    {
+    }
+}
---
+++ Application/Users/Queries/PasswordRequirements/PasswordRequirementsQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace PizzaApi.Application.Users.Queries.PasswordRequirements
+{
+    public class PasswordRequirementsQueryHandler(PasswordOptions options)
+        : IRequestHandler<PasswordRequirementsQuery, PasswordOptions>
+    {
+        private readonly PasswordOptions _options = options;
+
+        public async Task<PasswordOptions> Handle(PasswordRequirementsQuery query, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(_options);
+        }
+    }
+}
