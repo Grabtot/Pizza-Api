ad051b75c9dff20d34d8153c48a16aee30e2237f
Grabtot <dneshotkin@gmail.com>
2024-07-25 13:29:30 +0300

    added ingredient models #17

---
+++ Domain/Ingredients/ValueObjects/Allergen.cs
@@ -0,0 +1,37 @@
+﻿using PizzaApi.Domain.Common.Interfaces;
+
+namespace PizzaApi.Domain.Ingredients.ValueObjects
+{
+    public class Allergen : IAuditableEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        private string NormalizedName => Name.ToUpper();
+
+#pragma warning disable CS8618 
+        private Allergen() { }
+#pragma warning restore CS8618
+
+        public Allergen(string name, string? description = null)
+        {
+            Name = name;
+            Description = description;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(NormalizedName, Description);
+        }
+
+        public override bool Equals(object? obj)
+            => obj is Allergen other && other == this;
+
+        public static bool operator ==(Allergen? x, Allergen? y)
+            => x?.NormalizedName == y?.NormalizedName
+            && x?.Description == y?.Description;
+
+        public static bool operator !=(Allergen? x, Allergen? y) => !(x == y);
+    }
+}
