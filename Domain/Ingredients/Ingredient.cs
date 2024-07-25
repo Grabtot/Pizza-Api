912387db4093c3aa8d664c57b526039311c5ad21
Grabtot <dneshotkin@gmail.com>
2024-07-25 13:29:30 +0300

    added ingredient models #17

---
+++ Domain/Ingredients/Ingredient.cs
@@ -0,0 +1,23 @@
+﻿using PizzaApi.Domain.Common.Interfaces;
+using PizzaApi.Domain.Ingredients.ValueObjects;
+
+namespace PizzaApi.Domain.Ingredients
+{
+    public class Ingredient : IAuditableEntity
+    {
+        public Guid Id { get; init; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public required string Name { get; set; }
+        public List<Tag> Tags { get; set; }
+        public List<Allergen> Allergens { get; set; }
+        public double Price { get; set; }
+
+        public Ingredient()
+        {
+            Id = Guid.NewGuid();
+            Tags = [];
+            Allergens = [];
+        }
+    }
+}
---
+++ Domain/Ingredients/ValueObjects/Tag.cs
@@ -0,0 +1,39 @@
+﻿using PizzaApi.Domain.Common.Interfaces;
+using System.Drawing;
+
+namespace PizzaApi.Domain.Ingredients.ValueObjects
+{
+    public class Tag : IAuditableEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string Name { get; set; }
+        private string NormalizedName => Name.ToUpper();
+        public Color? Color { get; set; }
+
+#pragma warning disable CS8618 
+        private Tag() { }
+#pragma warning restore CS8618
+
+        public Tag(string name, Color? color = null)
+        {
+            Name = name;
+            Color = color;
+        }
+
+        public static readonly Tag Meat = new("Meat", System.Drawing.Color.Red);
+        public static readonly Tag Fish = new("Fish", System.Drawing.Color.Blue);
+
+        public override int GetHashCode()
+            => HashCode.Combine(NormalizedName);
+
+        public override bool Equals(object? obj)
+            => obj is Tag other && other == this;
+
+        public static bool operator ==(Tag? x, Tag? y)
+            => x?.NormalizedName == y?.NormalizedName;
+
+        public static bool operator !=(Tag? x, Tag? y) => !(x == y);
+
+    }
+}
