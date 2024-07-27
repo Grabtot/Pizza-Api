b745b86bfbbfbc5f2738b5023f92584261dbb5f9
Grabtot <dneshotkin@gmail.com>
2024-07-27 20:05:25 +0300

    Created `Delete` allergen endpoint #23

---
+++ Application/Allergens/Commands/DeleteAllergen/DeleteAllergenCommand.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace PizzaApi.Application.Allergens.Commands.DeleteAllergen
+{
+    public record DeleteAllergenCommand(string Name) : IRequest<ErrorOr<Success>>
+    {
+    }
+}
