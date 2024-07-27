4648673df7a45dad6951b654deab33a161baa83c
Grabtot <dneshotkin@gmail.com>
2024-07-27 19:42:37 +0300

    Created `Get` allergents endpoint #23

---
+++ Application/Allergens/Queries/GetAllAllergens/GetAllAllergensQuery.cs
@@ -0,0 +1,15 @@
+﻿using ErrorOr;
+using MediatR;
+using PizzaApi.Domain.Ingredients.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaApi.Application.Allergens.Queries.GetAllAllergens
+{
+    public class GetAllAllergensQuery : IRequest<ErrorOr<List<Allergen>>>
+    {
+    }
+}
---
+++ Application/Allergens/Queries/GetAllAllergens/GetAllAllergensQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using ErrorOr;
+using MediatR;
+using PizzaApi.Application.Common.Interfaces.Repositories;
+using PizzaApi.Domain.Ingredients.ValueObjects;
+
+namespace PizzaApi.Application.Allergens.Queries.GetAllAllergens
+{
+    public class GetAllAllergensQueryHandler(IAllergenRepository allergenRepository)
+        : IRequestHandler<GetAllAllergensQuery, ErrorOr<List<Allergen>>>
+    {
+        private readonly IAllergenRepository _allergenRepository = allergenRepository;
+
+        public async Task<ErrorOr<List<Allergen>>> Handle(GetAllAllergensQuery query, CancellationToken cancellationToken)
+        {
+            List<Allergen> allergens = await _allergenRepository.GetAll(cancellationToken);
+
+            return allergens;
+        }
+    }
+}
