194a2468e569c5075861871b8ee2f20645b6687e
Grabtot <dneshotkin@gmail.com>
2024-07-27 20:52:52 +0300

    Add `Put` allergen endpoint #23

---
+++ Application/Allergens/Commands/UpdateAllergen/UpdateAllergenCommand.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+using MediatR;
+using PizzaApi.Domain.Ingredients.ValueObjects;
+
+namespace PizzaApi.Application.Allergens.Commands.UpdateAllergen
+{
+    public record UpdateAllergenCommand(string CurrentName,
+        string? NewName,
+        string? Description) : IRequest<ErrorOr<Allergen>>;
+}
---
+++ Application/Allergens/Commands/UpdateAllergen/UpdateAllergenCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using ErrorOr;
+using MediatR;
+using PizzaApi.Application.Common.Interfaces.Repositories;
+using PizzaApi.Domain.Ingredients.ValueObjects;
+
+namespace PizzaApi.Application.Allergens.Commands.UpdateAllergen
+{
+    public class UpdateAllergenCommandHandler(IAllergenRepository allergenRepository)
+        : IRequestHandler<UpdateAllergenCommand, ErrorOr<Allergen>>
+    {
+        private readonly IAllergenRepository _allergenRepository = allergenRepository;
+
+        public async Task<ErrorOr<Allergen>> Handle(UpdateAllergenCommand command, CancellationToken cancellationToken)
+        {
+            Allergen? allergen = await _allergenRepository.FindAsync(command.CurrentName);
+
+            if (allergen == null)
+                return Error.NotFound(description: $"Allergen {command.CurrentName} not found");
+
+            if (command.NewName != null && await _allergenRepository.FindAsync(command.NewName) != null)
+                return Error.Conflict(description: $"Allergen {command.NewName} already exists");
+
+            allergen.Name = command.NewName ?? allergen.Name;
+            allergen.Description = command.Description;
+
+            return allergen;
+        }
+    }
+}
