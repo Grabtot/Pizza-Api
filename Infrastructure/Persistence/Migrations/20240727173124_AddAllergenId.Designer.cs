f6387f65112c256ced1d6faf0a6627c267a21c1f
Grabtot <dneshotkin@gmail.com>
2024-07-27 20:51:46 +0300

    add `_id` field to `Allergen` class #17

--- Infrastructure/Persistence/Migrations/20240727173124_AddAllergenId.Designer.cs
+++ Infrastructure/Persistence/Migrations/20240727173124_AddAllergenId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PizzaApi.Infrastructure.Persistence;
@@ -12,9 +13,11 @@ using PizzaApi.Infrastructure.Persistence;
 namespace PizzaApi.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(PizzaDbContext))]
-    partial class PizzaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240727173124_AddAllergenId")]
+    partial class AddAllergenId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
