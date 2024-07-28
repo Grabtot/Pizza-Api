0e06ae19c24e0a57bc5d48235d1096c23507adbf
Grabtot <dneshotkin@gmail.com>
2024-07-28 17:32:47 +0300

    Add repository for tags

---
+++ Application/Common/Interfaces/Repositories/ITagRepository.cs
@@ -0,0 +1,10 @@
+﻿using PizzaApi.Domain.Ingredients.ValueObjects;
+
+namespace PizzaApi.Application.Common.Interfaces.Repositories
+{
+    public interface ITagRepository : IRepository<Tag, string>
+    {
+        new Task<Tag?> FindAsync(string name);
+        Task<List<Tag>> GetAllAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Infrastructure/Persistence/Repositories/TagRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PizzaApi.Application.Common.Interfaces.Repositories;
+using PizzaApi.Domain.Ingredients.ValueObjects;
+
+namespace PizzaApi.Infrastructure.Persistence.Repositories
+{
+    public class TagRepository(PizzaDbContext context)
+        : Repository<Tag, string>(context), ITagRepository
+    {
+        public override async Task<Tag?> FindAsync(string name)
+        {
+            return await DbSet.FirstOrDefaultAsync(tag
+                => tag.NormalizedName == name.ToUpper());
+        }
+
+        public async Task<List<Tag>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await DbSet.ToListAsync(cancellationToken);
+        }
+    }
+}
