069c9be0896d26a4a5740d31c92eefb30815d40f
Grabtot <dneshotkin@gmail.com>
2024-07-28 18:26:10 +0300

    code cleanup

--- Application/Allergens/Queries/GetAllAllergens/GetAllAllergensQuery.cs
+++ Application/Allergens/Queries/GetAllAllergens/GetAllAllergensQuery.cs
@@ -1,11 +1,6 @@
 ﻿using ErrorOr;
 using MediatR;
 using PizzaApi.Domain.Ingredients.ValueObjects;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PizzaApi.Application.Allergens.Queries.GetAllAllergens
 {
